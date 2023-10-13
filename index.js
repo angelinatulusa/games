a9e10a49d453882c6c0d93720398e7a830fd2ae2
ang <a@gmail.com>
2023-10-13 12:45:56 +0300

    As an app developer I want to get a full details of a given game to show in my app [Delivers #186246644]

--- index.js
+++ index.js
@@ -3,8 +3,26 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+const games = [
+    {id: 1, name: "Wither 3", price: 29.99},
+    {id: 2, name: "Cyberpunk 2077", price: 59.99},
+    {id: 3, name: "Minecraft", price: 26.99},
+    {id: 4, name: "Counter-Strike: Global Offensive", price: 0},
+    {id: 5, name: "Roblox", price: 0},
+    {id: 6, name: "Grand Theft Auto V", price: 29.99},
+    {id: 7, name: "Valorant", price: 0},
+    {id: 8, name: "Forza Horizon 5", price: 59.99}
+]
+
 app.get('/games', (req,res) => {
-    res.send(["Wither 3", "Cyberpunk 2077" , "game3"])
+    res.send(games)
+})
+
+app.get('/games/:id', (req, res) => {
+    if(typeof games [req.params.id - 1] === 'undefined'){
+        return res.status(404).send({error: "Game not found"})
+    }
+    res.send(games[req.params.id - 1])
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
